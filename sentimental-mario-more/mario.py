0a6a7b39f505ffe51bf1a43800af103e9e4203a7
HabibMollah <habib.prodev@gmail.com>
2024-07-01 00:57:39 +0600

    solve sentimental mario more

---
+++ sentimental-mario-more/mario.py
@@ -0,0 +1,18 @@
+# TODO
+from cs50 import get_int
+
+while True:
+    height = get_int("Height: ")
+    if height >= 1 and height <= 8:
+        break
+
+
+def draw(height):
+    for i in range(height):
+        print(" " * (height - (i + 1)), end="")
+        print("#" * (i + 1), end="")
+        print("  ", end="")
+        print("#" * (i + 1))
+
+
+draw(height)
